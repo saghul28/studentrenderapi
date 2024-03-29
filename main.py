978118305be323de0972a0dad033321425cf8d08
saghul28 <saghulhameed30@gmail.com>
2024-03-29 13:50:32 +0530

    commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+import uvicorn
 app = FastAPI()
 
 
@@ -11,3 +11,6 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
